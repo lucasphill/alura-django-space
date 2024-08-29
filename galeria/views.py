0949bf1702c2118fd2a49f458a9ce271cc3831e4
lucasphill <lucaspscp@outlook.com>
2024-08-29 19:40:42 -0300

    Passando referencias

--- galeria/views.py
+++ galeria/views.py
@@ -1,18 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from galeria.models import Fotografia
 
 
 
 def index(request):
-    
-    dados = {
-        1: {'nome': 'Nebulosa de Carina',
-            'legenda': 'wbbbtelescope.org / Nasa / James Webb'},
-        2: {'nome': 'Galaxia NGC 1079',
-            'legenda': 'nasa.org / Nasa / Hubble'}
-    }
+    fotograifa = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {'cards':fotograifa})
 
-    return render(request, 'galeria/index.html', {'cards':dados})
-
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {'fotografia':fotografia})
