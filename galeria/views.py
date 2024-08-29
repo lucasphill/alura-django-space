40d2e65c016c15bd26e4eb63f6ad1f55e87d3940
lucasphill <lucaspscp@outlook.com>
2024-08-29 18:54:13 -0300

    Adição SQLIte

--- galeria/views.py
+++ galeria/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
 def index(request):
-    return render(request, 'galeria/index.html')
+    
+    dados = {
+        1: {'nome': 'Nebulosa de Carina',
+            'legenda': 'wbbbtelescope.org / Nasa / James Webb'},
+        2: {'nome': 'Galaxia NGC 1079',
+            'legenda': 'nasa.org / Nasa / Hubble'}
+    }
+
+    return render(request, 'galeria/index.html', {'cards':dados})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
